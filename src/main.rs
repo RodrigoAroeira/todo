11434a4ecb8dae4922ba1bcafdee2e7768410dbb
RodrigoAroeira <raroeirabr@gmail.com>
2025-10-16 11:40:49 -0300

    refactor: use dirs crate to resolve home directory - Added `dirs` dependency for cross-platform home directory resolution - Updated main.rs to use `dirs::home_dir` instead of manually reading   environment variables

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::path::PathBuf;
 
 use crate::app::App;
 
+use dirs::home_dir;
+
 mod action;
 mod app;
 mod globals;
@@ -10,9 +12,10 @@ mod screen_guard;
 mod tab;
 
 fn main() -> anyhow::Result<()> {
+    let home = home_dir().unwrap();
     let untreated_path = PathBuf::from(match std::env::args().nth(1) {
         Some(path) => path,
-        None => format!("{}/TODO", std::env::var("HOME").unwrap()),
+        None => format!("{}/TODO", home.display()),
     });
 
     let file_path = if untreated_path.is_dir() {
