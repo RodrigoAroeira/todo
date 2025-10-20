72fa676b3e80f55299c4a63ed294731791f26900
RodrigoAroeira <raroeirabr@gmail.com>
2025-10-20 08:09:40 -0300

    fix: replace unwrap with contextual error for missing home directory
    - Now home directory is checked only when no arguments are passed
    - Instead of unwrap, error is propagated with anyhow::Context and ?
      operator

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::path::PathBuf;
 
 use crate::app::App;
 
+use anyhow::Context;
 use dirs::home_dir;
 
 mod action;
@@ -12,11 +13,12 @@ mod screen_guard;
 mod tab;
 
 fn main() -> anyhow::Result<()> {
-    let home = home_dir().unwrap();
-    let untreated_path = PathBuf::from(match std::env::args().nth(1) {
-        Some(path) => path,
-        None => format!("{}/TODO", home.display()),
-    });
+    let untreated_path: PathBuf = match std::env::args().nth(1) {
+        Some(path) => PathBuf::from(path),
+        None => home_dir()
+            .context("Unable to find home directory")?
+            .join("TODO"),
+    };
 
     let file_path = if untreated_path.is_dir() {
         untreated_path.join("TODO")
